e18402249002469b3cf90ea70e0018ef66fc3cd4
fernandoggaitan <fernandoggaitan@gmail.com>
2025-11-04 19:13:24 -0300

    Variables de entorno .env

---
+++ src/routes/eventoRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+//Controlador
+const eventoController = require('../controllers/eventoController');
+
+module.exports = router;
